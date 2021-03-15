e62b188b1b2fb44f94b0df0a36efa6f19bb6cdba
Rudy <my4my4@gmail.com>
2021-03-15 19:35:37 +0900

    Update app.js

--- app.js
+++ app.js
@@ -33,6 +33,32 @@ function connectDB() {
     });
 }
 
+let authUser = function (database, id, password, callback) {
+    console.log('authUser 호출됨.');
+
+    // users 컬렉션 참조
+    let users = database.collection('users');
+
+    // 아이디와 비밀번호를 사용해 검색
+    users.find({
+        "id": id,
+        "password": password
+    }).toArray(function (err, docs) {
+        if (err) {
+            callback(err, null);
+            return;
+        }
+
+        if (docs.length > 0) {
+            console.log(`아이디 [%s], 비밀번호 [%s]가 일치하는 사용자 찾음.`, id, password);
+            callback(null, docs);
+        } else {
+            console.log("일치하는 사용자를 찾지 못함.");
+            callback(null, null);
+        }
+    });
+}
+
 // 오류 핸들러 모듈 로드
 const expressErrorHandler = require('express-error-handler');
 
@@ -81,6 +107,25 @@ let router = express.Router();
 // 로그인 라우팅 함수 - 데이터베이스의 정보와 비교
 router.route('/process/login').post(function (req, res) {
     console.log('/process/login 호출됨.');
+
+    let paramId = req.param('id');
+    let paramPassword = req.param('password');
+
+    if (database) {
+        authUser(database, paramId, paramPassword, function (err, docs) {
+            if (err) {
+                throw err;
+            }
+
+            if (docs) {
+                console.dir(docs);
+                let username = docs[0].name;
+                res.writeHead('200', {
+                    'Content-Type'
+                });
+            }
+        });
+    }
 });
 
 // 라우터 객체 등록
@@ -99,4 +144,7 @@ app.use(errorHandler);
 // ===== 서버 시작 ===== //
 http.createServer(app).listen(app.get('port'), function () {
     console.log('서버가 시작되었습니다. 포트 : ' + app.get('port'));
+
+    // 데이터베이스 연결
+    connectDB();
 });
